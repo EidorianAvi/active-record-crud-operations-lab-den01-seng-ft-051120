f08bac050d7c3485a9e48fbfabc7cdf2ea6c2bfb
Rene Avila <reneavila1993@gmail.com>
2020-05-26 04:29:43 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,11 +19,11 @@ def can_be_created_with_a_hash_of_attributes
       director: "George Roy Hill",
       lead: "Paul Newman",
       in_theaters: false
-  }m
+  }
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args ={title:"Home Alone",release_date: "1990"})
+def can_be_created_in_a_block(args ={title:"Home Alone",release_date "1990"})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990

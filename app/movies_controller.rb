ebd5b9cd8047bc9e5a55d8495be905f65cdfeabd
Rene Avila <reneavila1993@gmail.com>
2020-05-26 04:14:10 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,8 +30,8 @@ def can_be_created_in_a_block(title = "Home Alone", release_date = "1990")
   
   Movie.create do |m|
    m[:title] = title
-   m{:release_date] = release_date
-  end
+   m[:release_date] = release_date
+  end 
 end
 
 def can_get_the_first_item_in_the_database

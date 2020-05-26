6f1a490ab6aac63060a305085e365cff68d85a42
Rene Avila <reneavila1993@gmail.com>
2020-05-26 04:36:17 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,7 +61,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  Movie.where(release_date > 2002)
 end
 
 def can_be_found_updated_and_saved
@@ -97,5 +97,5 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.destroy
 end
